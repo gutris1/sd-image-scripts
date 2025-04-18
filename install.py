cecefa704239ac41cf9ec6f03410b90f71b3bd04
gutris1 <132797949+gutris1@users.noreply.github.com>
2025-04-18 22:25:48 +0700

    Update install.py

--- install.py
+++ install.py
@@ -1,16 +1,15 @@
 from pathlib import Path
 import subprocess
 
-def _Req():
-    def git(*args):
-        return subprocess.run(['git', *args], cwd=path, text=True, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL).stdout.strip()
+path = Path(__file__).parent
 
-    path = Path(__file__).parent
+def git(*args):
+    return subprocess.run(['git', *args], cwd=path, text=True, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL).stdout.strip()
+
+def req():
     url = git('config', '--get', 'remote.origin.url')
     local = git('rev-parse', 'HEAD')
-    remote = subprocess.run(['git', 'ls-remote', url, 'HEAD'], text=True, stdout=subprocess.PIPE).stdout.split()[0]
+    remote = subprocess.run(['git', 'ls-remote', url, 'HEAD'], text=True, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL).stdout.split()[0]
+    if local != remote: subprocess.run(['git', 'pull'], cwd=path, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
-    if local != remote:
-        subprocess.run(['git', 'pull'], cwd=path)
-
-_Req()
+req()
