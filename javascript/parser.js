8b9fcb5ce8c7dca5680d3feefd0a0e26f62c6df7
gutris1 <132797949+gutris1@users.noreply.github.com>
2025-08-09 13:24:01 +0700

    a

--- javascript/parser.js
+++ javascript/parser.js
@@ -1,18 +1,97 @@
 async function SharedImageParser(img) {
-  [
-    'EncryptInfo',
-    'Sha256Info',
-    'ExtrasInfo',
-    'PostProcessingInfo',
-    'NaiSourceInfo',
-    'SoftwareInfo'
-  ].forEach(k => window[`SharedParser${k}`] = '');
+  const decodeUserComment = (array) => {
+    const result = [];
+    let pos = 7;
 
-  const [prefix, base64] = img.src.split(','), b = Uint8Array.from(atob(base64), c => c.charCodeAt(0));
-  img.src = URL.createObjectURL(new Blob([b], { type: prefix.match(/data:(.*?);base64/)[1] }));
+    if (array[8] === 123) for (let i = pos; i < array.length; i += 2) { const a = array[i], b = array[i + 1]; result.push(a === 0 && b === 32 ? 32 : a * 256 + b); }
+    else {
+      for (let i = pos; i < array.length; i++) {
+        if (i === 7 && array[i] === 0) continue;
+        if (array[i] === 0) if (i + 1 < array.length && array[i + 1] === 0) { i++; continue; }
+        if (i + 1 < array.length) { const a = array[i], b = array[i + 1]; result.push(a === 0 && b === 32 ? 32 : a * 256 + b); i++; continue; }
+      }
+    }
 
-  const tags = ExifReader.load(b.buffer);
-  let output = '';
+    const output = new TextDecoder('utf-16').decode(new Uint16Array(result)).trim();
+    return output.replace(/^UNICODE[\x00-\x20]*/, '');
+  },
+
+  NovelAI = (input) => {
+    const NAIround = v => Math.round(v * 10000) / 10000,
+    NAIMultiplyRange = (start, multiplier) => res.slice(start).forEach(row => row[1] = NAIround(row[1] * multiplier)),
+    re_attention = /\{|\[|\}|\]|[^\{\}\[\]]+/gmu,
+    curly_bracket_multiplier = 1.05,
+    square_bracket_multiplier = 1 / 1.05;
+    let t = input.replaceAll('(', '\\(').replaceAll(')', '\\)').replace(/\\{2,}(\(|\))/gim, '\$1'),
+    res = [], curly_brackets = [], square_brackets = [], result = '';
+
+    for (const match of t.matchAll(re_attention)) {
+      let w = match[0];
+      if (w === '{') curly_brackets.push(res.length);
+      else if (w === '[') square_brackets.push(res.length);
+      else if (w === '}' && curly_brackets.length > 0) NAIMultiplyRange(curly_brackets.pop(), curly_bracket_multiplier);
+      else if (w === ']' && square_brackets.length > 0) NAIMultiplyRange(square_brackets.pop(), square_bracket_multiplier);
+      else res.push([w, 1.0]);
+    }
+
+    for (const pos of curly_brackets) NAIMultiplyRange(pos, curly_bracket_multiplier);
+    for (const pos of square_brackets) NAIMultiplyRange(pos, square_bracket_multiplier);
+    if (res.length === 0) res = [['', 1.0]];
+
+    let i = 0;
+    while (i + 1 < res.length) { if (res[i][1] === res[i + 1][1]) { res[i][0] += res[i + 1][0]; res.splice(i + 1, 1); } else { i++; }}
+    for (let i = 0; i < res.length; i++) { if (res[i][1] === 1.0) { result += res[i][0]; } else { result += `(${res[i][0]}:${res[i][1]})`; }}
+    return result;
+  },
+
+  swarmUI = (Sui, extraData = {}) => {
+    const parts = [],
+
+    Format = {
+      prompt: v => `${v}\n`,
+      negativeprompt: v => `Negative prompt: ${v}\n`,
+      steps: v => `Steps: ${v}`,
+      sampler: v => `Sampler: ${v.replace(/\beuler\b|\beuler(-\w+)?/gi, m => m.replace(/euler/i, 'Euler'))}`,
+      scheduler: v => `Schedule type: ${v}`,
+      cfgscale: v => `CFG scale: ${v}`,
+      seed: v => `Seed: ${v}`,
+      width: (_, obj) => obj.width && obj.height ? `Size: ${obj.width}x${obj.height}` : null,
+      model: v => `Model: ${v}`,
+      vae: v => `VAE: ${v.split('/').pop()}`
+    };
+
+    for (const [key, fn] of Object.entries(Format)) {
+      if (Sui[key] != null) {
+        const str = fn(Sui[key], Sui);
+        if (str) parts.push(str.replace(/\n$/, ''));
+      }
+    }
+
+    window.SharedParserSoftwareInfo = Sui?.swarm_version ? `SwarmUI ${Sui.swarm_version}` : '';
+
+    const ignoreKeys = Object.keys(Format).concat('swarm_version'),
+    otherParams = Object.entries(Sui).filter(([k]) => !ignoreKeys.includes(k)).map(([k, v]) => `${k}: ${v}`),
+    extraParams = Object.entries(extraData).map(([k, v]) => `${k}: ${v}`);
+    return [...parts, ...otherParams, ...extraParams].join(', ').trim();
+  };
+
+  ['EncryptInfo', 'Sha256Info', 'ExtrasInfo', 'PostProcessingInfo', 'NaiSourceInfo', 'SoftwareInfo']
+    .forEach(k => window[`SharedParser${k}`] = '');
+
+  let output = '', buff;
+
+  if (img.src.startsWith('data:')) {
+    const [prefix, base64] = img.src.split(','), b = Uint8Array.from(atob(base64), c => c.charCodeAt(0));
+    buff = b.buffer;
+    img.src = URL.createObjectURL(new Blob([b], { type: prefix.match(/data:(.*?);base64/)[1] }));
+  } else {
+    const src = (window.SDHubImg && window.SDHubImg.trim()) || img.src,
+    res = await fetch(src), blob = await res.blob();
+    buff = await blob.arrayBuffer();
+    img.src = URL.createObjectURL(blob);
+  }
+
+  const tags = ExifReader.load(buff);
 
   if (tags) {
     window.SharedParserEncryptInfo = tags.Encrypt?.description || '';
@@ -26,21 +105,21 @@ async function SharedImageParser(img) {
       if (tags.parameters.description.includes('sui_image_params')) {
         const parSing = JSON.parse(tags.parameters.description);
         const Sui = parSing['sui_image_params'];
-        output = _ConvertSwarmUI(Sui, {});
+        output = swarmUI(Sui, {});
       } else {
         output = tags.parameters.description;
       }
 
     } else if (tags.UserComment?.value) {
       const array = tags.UserComment.value;
-      const UserComments = _DecodeUserComment(array);
+      const UserComments = decodeUserComment(array);
       if (UserComments.includes('sui_image_params')) {
         const rippin = UserComments.trim().replace(/[\x00-\x1F\x7F]/g, '');
         const parSing = JSON.parse(rippin);
         if (parSing['sui_image_params']) {
           const Sui = parSing['sui_image_params'];
           const SuiExtra = parSing['sui_extra_data'] || {};
-          output = _ConvertSwarmUI(Sui, SuiExtra);
+          output = swarmUI(Sui, SuiExtra);
         }
       } else {
         if (UserComments.startsWith('Postprocess upscale')) {
@@ -57,8 +136,8 @@ async function SharedImageParser(img) {
       const nai = JSON.parse(tags.Comment.description);
       nai.sampler = 'Euler';
 
-      output = _ConvertNovelAI(nai['prompt']) +
-        '\nNegative prompt: ' + _ConvertNovelAI(nai['uc']) +
+      output = NovelAI(nai['prompt']) +
+        '\nNegative prompt: ' + NovelAI(nai['uc']) +
         '\nSteps: ' + nai['steps'] +
         ', Sampler: ' + nai['sampler'] +
         ', CFG scale: ' + parseFloat(nai['scale']).toFixed(1) +
@@ -80,85 +159,47 @@ async function SharedImageParser(img) {
   return output;
 }
 
-function _DecodeUserComment(array) {
-  const result = [];
-  let pos = 7;
+function SharedVersionParser(i) {
+  const m = i.match(/(?:,\s*)?Version:\s*([^\s,]+)/);
+  if (m) {
+    const v = m[1], T = 'Stable Diffusion WebUI'
 
-  if (array[8] === 123) for (let i = pos; i < array.length; i += 2) { const a = array[i], b = array[i + 1]; result.push(a === 0 && b === 32 ? 32 : a * 256 + b); }
-  else {
-    for (let i = pos; i < array.length; i++) {
-      if (i === 7 && array[i] === 0) continue;
-      if (array[i] === 0) if (i + 1 < array.length && array[i + 1] === 0) { i++; continue; }
-      if (i + 1 < array.length) { const a = array[i], b = array[i + 1]; result.push(a === 0 && b === 32 ? 32 : a * 256 + b); i++; continue; }
+    d = [
+      {
+        n: (v) => /^v?(\d)\.(\d{1,2})\.(\d)(-RC)?$/.test(v),
+        l: 'https://github.com/AUTOMATIC1111/stable-diffusion-webui',
+        t: (v) => `${T} — ${v}`,
+      },
+      {
+        n: (v) => v.includes('f2.0.1v1.10.1'),
+        l: 'https://github.com/lllyasviel/stable-diffusion-webui-forge',
+        t: (v) => `${T} Forge — ${v.split(/[\s,]/)[0]}`,
+      },
+      {
+        n: (v) => v.includes('f1.0.0v2-v1.10.1'),
+        l: 'https://github.com/Panchovix/stable-diffusion-webui-reForge',
+        t: (v) => `${T} ReForge — ${v.split(/[\s,]/)[0]}`,
+      },
+      {
+        n: (v) => v === 'classic',
+        l: 'https://github.com/Haoming02/sd-webui-forge-classic',
+        t: (v) => `${T} Forge Classic — ${v.split(/[\s,]/)[0]}`,
+      },
+    ],
+
+    w = d.find(({ n }) => n(v));
+
+    if (w) {
+      const { l, t } = w, f = typeof t === 'function' ? t(v) : `${t} ${v}`;
+      window.SharedParserSoftwareInfo = `
+        <a href='${l}' class='sd-image-parser-software' target='_blank' rel='noopener noreferrer'>
+          ${f}
+        </a>
+      `;
     }
   }
 
-  const output = new TextDecoder('utf-16').decode(new Uint16Array(result)).trim();
-
-  return output.replace(/^UNICODE[\x00-\x20]*/, '');
-}
-
-function _ConvertNovelAI(input) {
-  const NAIround = v => Math.round(v * 10000) / 10000;
-  const NAIMultiplyRange = (start, multiplier) => res.slice(start).forEach(row => row[1] = NAIround(row[1] * multiplier));
-  const re_attention = /\{|\[|\}|\]|[^\{\}\[\]]+/gmu;
-  let t = input.replaceAll('(', '\\(').replaceAll(')', '\\)').replace(/\\{2,}(\(|\))/gim, '\$1');
-  let res = [];
-  let curly_brackets = [];
-  let square_brackets = [];
-  const curly_bracket_multiplier = 1.05;
-  const square_bracket_multiplier = 1 / 1.05;
-
-  for (const match of t.matchAll(re_attention)) {
-    let w = match[0];
-    if (w === '{') curly_brackets.push(res.length);
-    else if (w === '[') square_brackets.push(res.length);
-    else if (w === '}' && curly_brackets.length > 0) NAIMultiplyRange(curly_brackets.pop(), curly_bracket_multiplier);
-    else if (w === ']' && square_brackets.length > 0) NAIMultiplyRange(square_brackets.pop(), square_bracket_multiplier);
-    else res.push([w, 1.0]);
-  }
-
-  for (const pos of curly_brackets) NAIMultiplyRange(pos, curly_bracket_multiplier);
-  for (const pos of square_brackets) NAIMultiplyRange(pos, square_bracket_multiplier);
-  if (res.length === 0) res = [['', 1.0]];
-
-  let i = 0;
-  while (i + 1 < res.length) { if (res[i][1] === res[i + 1][1]) { res[i][0] += res[i + 1][0]; res.splice(i + 1, 1); } else { i++; }}
-
-  let result = '';
-  for (let i = 0; i < res.length; i++) { if (res[i][1] === 1.0) { result += res[i][0]; } else { result += `(${res[i][0]}:${res[i][1]})`; }}
-
-  return result;
-}
-
-function _ConvertSwarmUI(Sui, extraData = {}) {
-  let output = '';
-
-  if (Sui.prompt) output += `${Sui.prompt}\n`;
-  if (Sui.negativeprompt) output += `Negative prompt: ${Sui.negativeprompt}\n`;
-  if (Sui.steps) output += `Steps: ${Sui.steps}, `;
-  if (Sui.sampler) {
-    Sui.sampler = Sui.sampler.replace(/\beuler\b|\beuler(-\w+)?/gi, (match) => { return match.replace(/euler/i, 'Euler'); });
-    output += `Sampler: ${Sui.sampler}, `;
-  }
-  if (Sui.scheduler) output += `Schedule type: ${Sui.scheduler}, `;
-  if (Sui.cfgscale) output += `CFG scale: ${Sui.cfgscale}, `;
-  if (Sui.seed) output += `Seed: ${Sui.seed}, `;
-  if (Sui.width && Sui.height) output += `Size: ${Sui.width}x${Sui.height}, `;
-  if (Sui.model) output += `Model: ${Sui.model}, `;
-  if (Sui.vae) { const vaeParts = Sui.vae.split('/'); output += `VAE: ${vaeParts[vaeParts.length - 1]}, `; }
-
-  window.SharedParserSoftwareInfo = Sui?.swarm_version ? `SwarmUI ${Sui.swarm_version}` : '';
-  output = output.trim().replace(/,$/, '');
-
-  let otherParams = Object.entries(Sui).filter(([k]) => ![
-    'prompt','negativeprompt','steps','sampler','scheduler','cfgscale','seed','width','height','model','vae','swarm_version'
-  ].includes(k)).map(([k,v]) => `${k}: ${v}`).join(', ');
-
-  let extraParams = Object.entries(extraData).map(([key, value]) => `${key}: ${value}`).join(', ');
-  if (otherParams || extraParams) output += (output ? ', ' : '') + [otherParams, extraParams].filter(Boolean).join(', ');
-
-  return output.trim();
+  return i;
 }
 
 async function SharedModelsFetch(i, timeout = 60000) {
@@ -171,22 +212,22 @@ async function SharedModelsFetch(i, timeout = 60000) {
 
   return await Promise.race([
     (async () => {
-      const Cat = { checkpoint: [], vae: [], lora: [], embed: [] };
-
-      const EmbedNames = new Set();
-      const LoraNames = new Set();
-      const HashesDict = {};
-
       let FetchedModels = '';
 
-      const Link = 'sd-image-parser-link';
-      const nonLink = 'sd-image-parser-nonlink';
+      const Cat = { checkpoint: [], vae: [], lora: [], embed: [] },
 
-      const TIHashesSearchLink = async (n, h) => h
+      EmbedNames = new Set(),
+      LoraNames = new Set(),
+      HashesDict = {},
+
+      Link = 'sd-image-parser-link',
+      nonLink = 'sd-image-parser-nonlink',
+
+      TIHashesSearchLink = async (n, h) => h
         ? `<a class='${Link}' href='https://civitai.com/search/models?sortBy=models_v9&query=${h}' target='_blank'>${n}</a>`
-        : `<span class='${nonLink}'>${n}: ${h}</span>`;
+        : `<span class='${nonLink}'>${n}: ${h}</span>`,
 
-      const FetchingModels = async (n, h, isThat = false) => {
+      FetchingModels = async (n, h, isThat = false) => {
         const N = `<span class='${nonLink}'>${n}${isThat ? '' : `: ${h}`}</span>`;
         if (!h) return N;
         try {
@@ -198,9 +239,9 @@ async function SharedModelsFetch(i, timeout = 60000) {
         } catch {
           return N;
         }
-      };
+      },
 
-      const FetchLora = async (n, h) => {
+      FetchLora = async (n, h) => {
         if (!n || !h || LoraNames.has(n)) return;
         LoraNames.add(n);
         const Hashes = async () => {
@@ -214,9 +255,9 @@ async function SharedModelsFetch(i, timeout = 60000) {
         };
         const f = await Hashes();
         Cat.lora.push(f);
-      };
+      },
 
-      const FetchEmbed = async (n, h) => {
+      FetchEmbed = async (n, h) => {
         if (!n || !h || EmbedNames.has(n)) return;
         EmbedNames.add(n);
         const Hashes = async () => {
@@ -230,31 +271,31 @@ async function SharedModelsFetch(i, timeout = 60000) {
         };
         const f = await Hashes();
         Cat.embed.push(f);
-      };
+      },
 
-      const FetchResult = (l, m) => {
+      FetchResult = (l, m) => {
         return `
           <div class='sd-image-parser-modeloutput-line'>
             <div class='sd-image-parser-modeloutput-label'>${l}</div>
             <div class='sd-image-parser-modeloutput-hashes'>${m.join(' ')}</div>
           </div>
         `;
-      };
+      },
 
-      const modelEX = i.includes('Model: "') ? i.match(/Model:\s*"?([^"]+)"/) : i.match(/Model:\s*([^,]+)/);
-      const modelHashEX = i.match(/Model hash:\s*([^,]+)/);
-      const vaeEX = i.match(/VAE:\s*([^,]+)/);
-      const vaeHashEX = i.match(/VAE hash:\s*([^,]+)/);
-      const loraHashEX = i.match(/Lora hashes:\s*"([^"]+)"/);
-      const tiHashEX = i.match(/TI hashes:\s*"([^"]+)"/);
-      const hashesIndex = i.indexOf('Hashes:');
-      const hashesEX = hashesIndex !== -1 ? i.slice(hashesIndex).match(/Hashes:\s*(\{.*?\})(,\s*)?/) : null;
+      modelEX = i.includes('Model: "') ? i.match(/Model:\s*"?([^"]+)"/) : i.match(/Model:\s*([^,]+)/),
+      modelHashEX = i.match(/Model hash:\s*([^,]+)/),
+      vaeEX = i.match(/VAE:\s*([^,]+)/),
+      vaeHashEX = i.match(/VAE hash:\s*([^,]+)/),
+      loraHashEX = i.match(/Lora hashes:\s*"([^"]+)"/),
+      tiHashEX = i.match(/TI hashes:\s*"([^"]+)"/),
+      hashesIndex = i.indexOf('Hashes:'),
+      hashesEX = hashesIndex !== -1 ? i.slice(hashesIndex).match(/Hashes:\s*(\{.*?\})(,\s*)?/) : null;
 
       if (modelEX) {
-        const modelValue = modelEX[1];
-        const modelHash = modelHashEX ? modelHashEX[1] : null;
-        const vaeValue = vaeEX ? vaeEX[1] : null;
-        const vaeHash = vaeHashEX ? vaeHashEX[1] : null;
+        const modelValue = modelEX[1],
+        modelHash = modelHashEX ? modelHashEX[1] : null,
+        vaeValue = vaeEX ? vaeEX[1] : null,
+        vaeHash = vaeHashEX ? vaeHashEX[1] : null;
 
         if (modelHash || vaeValue || vaeHash) Cat.checkpoint.push({ n: modelValue, h: modelHash });
         if (vaeValue || vaeHash) Cat.vae.push({ n: vaeValue, h: vaeHash });
